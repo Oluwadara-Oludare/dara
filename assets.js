ddf62a2705eeeded9976b3f5436585731c4744f7
Oluwadara-Oludare <dotundara91@gmail.com>
2024-10-04 20:32:25 +0100

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,134 @@
+
+
+export const blog_data = [{
+    id: 1,
+    title: 'Circuit Analysis',
+    description: 'Mesh analysis provides another general procedure for analyzing circuits, using mesh currents as the circuit variables. Using mesh currents instead of element currents as circuit variables',
+    image: '/assets/IMG_8283.jpg',
+    date: Date.now(),
+    category: 'Electricity',
+    author: 'John Sullivan',
+    author_img: '/assets/IMG_8956.jpg',
+},
+{
+    id: 2,
+    title: 'Nodal Analysis',
+    description: 'The proof of Norton’s theorem will be given in the next section.For now, we are mainly concerned with how to get and We find in the same way we find I',
+    image: '/assets/IMG_8285.jpg',
+    date: Date.now(),
+    category: 'Electricity',
+    author: 'James Rote',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 3,
+    title: 'Complex Numbers',
+    description: 'We have seen that is the frequency spectrum of the signal . Thus the components of give a resolution of the -periodic function into simple (complex) harmonics.Here one should use only n’s that are much smaller than , to avoid aliasing.',
+    image: '/assets/IMG_8286.jpg',
+    date: Date.now(),
+    category: 'Mathematics',
+    author: 'Arthur Yates',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 4,
+    title: 'Structures',
+    description: 'The electrostatic potential satisfies Laplace’s equation in any region free of charges. Also the heat equation (Sec. 12.5) reduces to Laplace’s equation if the temperature u is time-independent (“steady-state case”).',
+    image: '/assets/IMG_8290.jpg',
+    date: Date.now(),
+    category: 'Engineering',
+    author: 'Arthur Y. Daniels',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 5,
+    title: 'Beams',
+    description: 'Partial sums of these series can now be used for computing approximate values of the inner and outer potential.Also, it is interesting to see that far away from the sphere the potential is approximately that of a point charge,namely, . ',
+    image: '/assets/IMG_8292.jpg',
+    date: Date.now(),
+    category: 'Engineering',
+    author: 'Elijah Fredricks',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 6,
+    title: 'Color Coding',
+    description: 'Norton’s theorem states that a linear two-terminal circuit can be replaced by an equivalent circuit consisting of a current source IN in parallel with a resistor RN, where IN is the short-circuit current through the terminals and RN is the input or equivalent resistance at the terminals when the independent sources are turned off.',
+    image: '/assets/IMG_8293.jpg',
+    date: Date.now(),
+    category: 'Electricity',
+    author: 'Amos Saals',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 7,
+    title: 'Surds',
+    description: 'Present. Clearly, using the devices developed in Section 6.6.3 will allow us to create the desired analog computer circuit. We will need the integrator circuits (possibly combined with a summing capability) and one or more inverter circuits.',
+    image: '/assets/IMG_8296.jpg',
+    date: Date.now(),
+    category: 'Mathematics',
+    author: 'Amos Saals',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 8,
+    title: 'Sytems',
+    description: 'So far we discussed the algebraic manipulation of complex numbers. Consider the geometric representation of complex numbers, which is of great practical importance. We choose two perpendicular coordinate axes, the horizontal x-axis, called the real axis, and the vertical y-axis, called the imaginary axis. On both axes we choose the same unit of length (Fig. 318). This is called a Cartesian coordinate system.',
+    image: '/assets/IMG_8297.jpg',
+    date: Date.now(),
+    category: 'Engineering',
+    author: 'Susy Stain',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 9,
+    title: 'Electronic Configurations',
+    description: 'An inductor is a passive element designed to store energy in its magnetic field. Inductors find numerous applications in electronic and power systems. They are used in power supplies, transformers, radios, TVs, radars, and electric motors.',
+    image: '/assets/IMG_8297.jpg',
+    date: Date.now(),
+    category: 'Electricity',
+    author: 'James Januery',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 10,
+    title: 'Tensor Analysis',
+    description: 'If we want to investigate a function for large we may now set and investigate in a neighborhood of We define to be analytic or singular at infinity if is analytic or singular, respectively, at We also define (4) if this limit exists',
+    image: '/assets/IMG_8299.jpg',
+    date: Date.now(),
+    category: 'Enginneering',
+    author: 'James Lutuniu',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 11,
+    title: 'Electronic Configurations II',
+    description: 'Any conductor of electric current has inductive properties and may be regarded as an inductor. But in order to enhance the inductive effect, a practical inductor is usually formed into a cylindrical coil with many turns of conducting wire,',
+    image: '/assets/IMG_8301.jpg',
+    date: Date.now(),
+    category: 'Electricity',
+    author: 'Daniel Austin',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+{
+    id: 12,
+    title: 'Data Structures',
+    description: 'We consider the problem of computing all the eigenvalues of a real symmetric matrix discussing a method widely used in practice. In the first stage we reduce the given matrix stepwise to a tridiagonal matrix, that is, a matrix having all its nonzero entries on the main diagonal and in the positions immediately adjacent to the main diagonal (such as in Fig. 450, Third Step)',
+    image: '/assets/IMG_8303.jpg',
+    date: Date.now(),
+    category: 'Enginneering',
+    author: 'James Januery',
+    author_img: '/assets/IMG_8956.jpg',
+
+},
+]
